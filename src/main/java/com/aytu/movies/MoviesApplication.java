d8dd6c5cd4b5e584a54271826ba2320c147461a3
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2024-03-14 15:39:46 +0300

    Added Review controller, model, repository, service

--- src/main/java/com/aytu/movies/MoviesApplication.java
+++ src/main/java/com/aytu/movies/MoviesApplication.java
@@ -2,6 +2,8 @@ package com.aytu.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -10,4 +12,16 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 }
